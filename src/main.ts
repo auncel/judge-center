e83a997bd74ea2e0b03120e55224ee871935d07c
yidafu <tiaozuwushi@qq.com>
2020-04-24 22:06:47 +0800

    refactor: change micro server to http

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,8 @@ import { Transport } from '@nestjs/common/enums/transport.enum';
 import { MICRO_SERVICE_TCP_HOST, MICRO_SERVICE_TCP_PORT } from './config/index'
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice(AppModule, {
-    transport: Transport.TCP,
-    options: {
-      host: MICRO_SERVICE_TCP_HOST,
-      port: MICRO_SERVICE_TCP_PORT,
-      retryAttempts: 3,
-    },
-  });
-  await app.listen(() => console.log('[micro service][judge center]: starting!'));
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3300, () => console.log('[micro service][judge center]: starting!'));
 }
 
 bootstrap();
